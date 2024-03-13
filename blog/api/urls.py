de3b1339bb70a57b1dd238543e85237ac59c376a
Yaman Malla <yymm.an1423@gmail.com>
2024-03-13 12:33:37 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,7 +4,8 @@ from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
+from rest_framework.routers import DefaultRouter
 
 
 schema_view = get_schema_view(
@@ -17,10 +18,20 @@ schema_view = get_schema_view(
     public=True,
 )
 
+router = DefaultRouter()
+
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
     re_path(
@@ -33,7 +44,5 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+    path("", include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
