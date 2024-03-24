daa2cb04bd7e42dd74185c28a8bddf6d91c05741
Yaman Malla <yymm.an1423@gmail.com>
2024-03-24 14:14:10 +0000

    Finish filtering

--- blog/api/views.py
+++ blog/api/views.py
@@ -15,6 +15,10 @@ from rest_framework import generics, viewsets
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Q
+from django.utils import timezone
+from datetime import timedelta
+from django.http import Http404
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -27,7 +31,44 @@ class PostViewSet(viewsets.ModelViewSet):
         return PostDetailSerializer
 
 
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
+
+        elif not self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(
+                Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+            )
+
+        time_period_name = self.kwargs.get("period_name")
+
+        if not time_period_name:
+            # no further filtering required
+            return queryset
+
+        if time_period_name == "new":
+            return queryset.filter(
+                published_at__gte=timezone.now() - timedelta(hours=1)
+            )
+        elif time_period_name == "today":
+            return queryset.filter(
+                published_at__date=timezone.now().date(),
+            )
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(
+                f"Time period {time_period_name} is not valid, should be "
+                f"'new', 'today' or 'week'"
+            )
+
+
     @method_decorator(cache_page(120))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
     def list(self, *args, **kwargs):
         return super(PostViewSet, self).list(*args, **kwargs)
 
